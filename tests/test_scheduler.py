a712dd7c8ef62baee53730ec329037605fd0cdcb
Alexis Janon <kardyne@users.no-reply.github.com>
2020-04-14 17:27:27 +0200

    test_scheduler: add global fairness test
    This new test ensures that there is the same number of teams having more home
    games and having less home games.
    This ensures that no team has much less or much more home games.

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,6 +1,7 @@
 """ This module contains tests for the scheduler. """
+import collections
 import itertools
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 import pytest
 
@@ -74,3 +75,32 @@ def test_length(team_count: int, return_game: bool) -> None:
             len(flattened_schedule)
             == (normalized_team_count * (normalized_team_count - 1)) // 2
         )
+
+
+@pytest.mark.parametrize("team_count", list(range(1, 10)))
+@pytest.mark.parametrize("return_game", [False, True])
+def test_global_fairness(team_count: int, return_game: bool) -> None:
+    """ Checks that the schedule is globally fair.
+
+    Basically, it checks that there is the same number of teams having more
+    home games and less home games.
+
+    Parameters
+    ----------
+    team_count : int
+        Number of teams generated for the schedule. Must be greater than zero.
+    return_game : bool
+        Whether to generate return games.
+    """
+    flattened_schedule: List[Game] = create_flattened_schedule(team_count, return_game)
+    home_count: Dict[Team, int] = {}
+    for game in flattened_schedule:
+        home_count[game[0]] = home_count.get(game[0], 0) + 1
+    home_occurences: collections.Counter = collections.Counter(home_count.values())
+    # home_occurences.values() returns the occurrence count as a list
+    # We then transform this list in a set, as a set removes duplicate values
+    # If the set only has 1 element, this means that all occurrences have the
+    # same count.
+    print(home_count)
+    print(home_occurences)
+    assert len(set(home_occurences.values())) == 1
